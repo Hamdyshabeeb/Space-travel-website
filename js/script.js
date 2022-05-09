49b3d2f23ac34112857ab3f9e7d45979323cd167
Hamdy Shabeeb <hamdysh2010@hotmail.com>
2022-05-09 16:38:04 +0200

    docs:add documentation about nav fuctionality

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const navToggleButton = document.querySelector('.nav-toggle');
 const navMenu = document.querySelector('.primary-navigation');
 
+// add fuctionality to nav button
 navToggleButton.addEventListener('click', () => {
 	navToggleButton.classList.toggle('open');
 	navMenu.classList.toggle('open');
