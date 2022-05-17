e617251d99243d85f46d4e0d2da5bb7611840f2c
Hamdy Shabeeb <hamdysh2010@hotmail.com>
2022-05-17 22:15:14 +0200

    Feat:add fuctionality to silder

--- js/script.js
+++ js/script.js
@@ -1,8 +1,38 @@
 const navToggleButton = document.querySelector('.nav-toggle');
 const navMenu = document.querySelector('.primary-navigation');
-
+const tabList = document.querySelectorAll('.tab');
 // add fuctionality to nav button
 navToggleButton.addEventListener('click', () => {
 	navToggleButton.classList.toggle('open');
 	navMenu.classList.toggle('open');
 });
+
+tabList.forEach((tab) => {
+	tab.addEventListener('click', carousel);
+});
+
+function carousel(e) {
+	const currentTab = e.target;
+	const activeTab = [...tabList].filter((tab) =>
+		tab.classList.contains('active')
+	)[0];
+	const carouselList = document.querySelectorAll('.carousel-inner');
+	// remove active state;
+	activeTab.classList.remove('active');
+	// add active state;
+	currentTab.classList.add('active');
+	// get number inside current tab;
+	const tabNumber = +currentTab.dataset.number;
+	carouselList.forEach((carousel) => {
+		sliding(carousel, tabNumber);
+	});
+}
+
+function sliding(ele, num) {
+	//get the width of single carousel item;
+	const itemWidth = ele.firstElementChild.getBoundingClientRect().width;
+
+	//the total displacement  length;
+	const displacement = num * itemWidth;
+	ele.style.transform = `translateX(${-1 * displacement}px)`;
+}
